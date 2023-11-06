a81f493ea2c2942a4cc2371c63b59766e9d81986
fatimashehab99 <fatima_shehab_sh@outlook.com>
2023-11-06 15:14:01 +0200

    tables.py

--- scripts/common/base.py
+++ scripts/common/base.py
@@ -8,8 +8,8 @@ database_url="postgresql+psycopg2://postgres:root@localhost/practice-ETL";
 
 try:
  engine=create_engine(database_url)
- with engine.begin() as connection:
-     print("Connected to the database")
+ """with engine.begin() as connection:
+     print("Connected to the database")"""
  
 except Exception as e:
     print("Error while connecting to the database:",str(e))
---
+++ scripts/common/tables.py
@@ -0,0 +1,19 @@
+from base import Base
+from sqlalchemy import cast,Column,Integer,String,Date
+from sqlalchemy.orm import column_property
+
+class PprRawAll(Base):
+    __tablename__ = "ppr_raw_all"  # Define the table name
+    id = Column(Integer, primary_key=True)
+    date_of_sale = Column(String(55))
+    address = Column(String(255))
+    postal_code = Column(String(55))
+    county = Column(String(55))
+    price = Column(String(55))
+    description = Column(String(255))
+    transaction_id=column_property(
+        date_of_sale + "_" + address + "_" + county + "_" + price
+    )
+    
+    
+    
